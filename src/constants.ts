87209d93500122b5ffba21a120249b5567bdf27b
thebongy <rishit.bansal0@gmail.com>
2019-12-18 18:43:37 +0530

    feat: Base protocol messages

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const constants = {
+    requestType: {
+        moduleRegistration: 'moduleRegistration',
+        functionCall: 'functionCall',
+        registerHook: 'registerHook',
+        triggerHook: 'triggerHook',
+        declareFunction: 'declareFunction',
+    }
+};
+
+export default constants;
