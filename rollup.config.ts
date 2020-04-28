6784b42accf03660856eeb0cd16c5ee0c0455c39
thebongy <rishit.bansal0@gmail.com>
2020-04-28 17:25:26 +0530

    fix: Build only cjs style module, juno-node won't run in a browser

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,11 +12,11 @@ const libraryName = 'juno-node'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.main, name: camelCase(libraryName), format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['net'],
   watch: {
     include: 'src/**',
   },
