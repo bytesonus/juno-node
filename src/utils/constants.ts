2ed82093a8d25004cb09dee48fec65b92e4f8219
thebongy <rishit.bansal0@gmail.com>
2020-05-08 23:01:31 +0530

    fix: Throw unkown function error code when function not found in module. Support returning falsy values from a juno function

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -19,9 +19,21 @@ export const RequestTypes = {
 };
 
 export const ResponseTypes = {
+	Error: 0,
 	ModuleRegistered: 2,
 	FunctionResponse: 4,
 	HookRegistered: 6,
 	HookTriggered: 8,
 	FunctionDeclared: 10
+};
+
+export const ErrorTypes = {
+	MalformedRequest: 0,
+	InvalidRequestId: 1,
+	UnkownRequest: 2,
+	UnregisteredModule: 3,
+	UnkownModule: 4,
+	UnkownFunction: 5,
+	InvalidModuleId: 6,
+	DuplicateModule: 7,
 };
